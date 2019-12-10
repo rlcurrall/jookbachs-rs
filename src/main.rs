e51a3f3a2181ec00d572aa06eb0f7df2eea7fca2
Robb Currall <robert@currall.net>
2019-12-09 19:10:01 -0500

    Add a use statement to clean up code

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use actix_web::{App, HttpServer};
 
 mod controllers;
 
+use controllers::{album_controller, artist_controller, track_controller};
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
@@ -16,17 +18,17 @@ async fn main() -> std::io::Result<()> {
                     .finish(),
             )
             // Album Routes
-            .service(controllers::album_controller::get_all_albums)
-            .service(controllers::album_controller::get_album_by_id)
-            .service(controllers::album_controller::get_album_tracks)
+            .service(album_controller::get_all_albums)
+            .service(album_controller::get_album_by_id)
+            .service(album_controller::get_album_tracks)
             // Artist Routes
-            .service(controllers::artist_controller::get_all_artists)
-            .service(controllers::artist_controller::get_artist_by_id)
-            .service(controllers::artist_controller::get_artist_tracks)
+            .service(artist_controller::get_all_artists)
+            .service(artist_controller::get_artist_by_id)
+            .service(artist_controller::get_artist_tracks)
             // Track Routes
-            .service(controllers::track_controller::get_all_tracks)
-            .service(controllers::track_controller::get_track_by_id)
-            .service(controllers::track_controller::stream_track)
+            .service(track_controller::get_all_tracks)
+            .service(track_controller::get_track_by_id)
+            .service(track_controller::stream_track)
     })
     .bind("127.0.0.1:8080")?
     .start()
